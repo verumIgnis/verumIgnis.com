9e65e3db959f9dab1773966b2435819ea1795686
verumIgnis <verumIgnisyt@gmail.com>
2022-12-31 01:17:13 +0000

    minor fixes and human prompt

--- venv/app.py
+++ venv/app.py
@@ -4,18 +4,28 @@ from PIL import Image, ImageDraw
 
 app = Flask(__name__)
 
-base64_to_rgb = {'A':0,'B':4,'C':8,'D':12,'E':16,'F':20,'G':24,'H':28,'I':32,'J':36,'K':40,'L':44,'M':48,'N':52,'O':56,'P':60,'Q':64,'R':68,'S':72,'T':76,'U':80,'V':84,'W':88,'X':92,'Y':96,'Z':100,'a':104,'b':108,'c':112,'d':116,'e':120,'f':124,'g':128,'h':132,'i':136,'j':140,'k':144,'l':148,'m':152,'n':156,'o':160,'p':164,'q':168,'r':172,'s':176,'t':180,'u':184,'v':188,'w':192,'x':196,'y':200,'z':204,'0':208,'1':212,'2':216,'3':220,'4':224,'5':228,'6':232,'7':236,'8':240,'9':244,'_':248,';':252,'¬':255}
+base64_to_rgb = {'A': 0, 'B': 4, 'C': 8, 'D': 12, 'E': 16, 'F': 20, 'G': 24, 'H': 28, 'I': 32, 'J': 36, 'K': 40,
+                 'L': 44, 'M': 48, 'N': 52, 'O': 56, 'P': 60, 'Q': 64, 'R': 68, 'S': 72, 'T': 76, 'U': 80, 'V': 84,
+                 'W': 88, 'X': 92, 'Y': 96, 'Z': 100, 'a': 104, 'b': 108, 'c': 112, 'd': 116, 'e': 120, 'f': 124,
+                 'g': 128, 'h': 132, 'i': 136, 'j': 140, 'k': 144, 'l': 148, 'm': 152, 'n': 156, 'o': 160, 'p': 164,
+                 'q': 168, 'r': 172, 's': 176, 't': 180, 'u': 184, 'v': 188, 'w': 192, 'x': 196, 'y': 200, 'z': 204,
+                 '0': 208, '1': 212, '2': 216, '3': 220, '4': 224, '5': 228, '6': 232, '7': 236, '8': 240, '9': 244,
+                 '_': 248, ';': 252, '¬': 255}
+
 
 def draw_pixel(draw, x, y, color):
     draw.rectangle((x * 10, y * 10, x * 10 + 10, y * 10 + 10), fill=color)
+
 
 @app.route('/')
 def index():
     return send_file("index.html", mimetype="text/html")
 
+
 @app.route('/cool-video')
 def cool_video():
     return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
 
 @app.route('/bitmapgen/<input_string>')
 def bitmapgen(input_string):
@@ -57,6 +67,7 @@ def bitmapgen(input_string):
     image.save(response.stream, format='PNG')
     return response
 
+
 @app.route('/<path:filename>')
 def serve(filename):
     if "download" in filename:
@@ -71,10 +82,11 @@ def serve(filename):
         except FileNotFoundError:
             abort(404)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return send_file("404.html", mimetype="text/html"), 404
 
+
 if __name__ == '__main__':
     app.run()
-
